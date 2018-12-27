2c9759592ac24752b42dd714cc73bb1aa272f7bd
SuvarnG <40597928+SuvarnG@users.noreply.github.com>
2018-12-27 18:12:07 +0530

    Initial commit

---
+++ CRUDUsingEntityAtul/Controllers/EmployeeController.cs
@@ -0,0 +1,53 @@
+﻿using CRUDUsingEntityAtul.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDUsingEntityAtul.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        AtulDatabaseEntities db = new AtulDatabaseEntities();
+        public ActionResult Index()
+        {
+            var data = db.Employees.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            db.Employees.Add(emp);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var EditRow = db.Employees.Where(model => model.ID == id).FirstOrDefault();
+            return View(EditRow);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee emp)
+        {
+            db.Entry(emp).State = EntityState.Modified;
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Delete(Employee emp)
+        {
+            
+            db.Entry(emp).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
